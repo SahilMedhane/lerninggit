9329020198781974e459958a0f2976c0ccd91753
Sahil Bhalchandra Medhane <135269339+SahilMedhane@users.noreply.github.com>
2023-06-04 01:37:11 +0530

    Add files via upload

---
+++ program4.java
@@ -0,0 +1,31 @@
+//for -6 value it prints number is greater than 5 means elses output
+
+class Question4{
+	public static void main(String[] args){
+		
+		int x = -6;
+
+		if(x==0){
+			System.out.println("zero");
+
+		}else if(x==1){
+			System.out.println("one");
+
+		}else if(x==2){
+			System.out.println("two");
+
+		}else if(x==3){
+			System.out.println("three");
+
+		}else if(x==4){
+			System.out.println("four");
+
+		}else if (x==5){
+			System.out.println("five");
+
+		}else{
+			System.out.println("number is greater than 5");
+
+		}
+	}
+}
---
+++ question3.java
@@ -0,0 +1,18 @@
+class Question3 {
+	public static void main(String[] args){
+
+		int x=5;
+
+		if(x>0){
+			System.out.println("it is a positive number");
+
+		}else if(x<0){
+			System.out.println("it is a negative number");
+			
+		}else{
+			System.out.println("it is 0");
+
+		}
+	}
+
+}
